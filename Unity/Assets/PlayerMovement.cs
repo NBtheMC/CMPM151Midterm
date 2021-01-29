2a12c849e3cefaa9147c31f35591c357a4a85bb2
NBtheMC <nvbhusha@ucsc.edu>
2021-01-28 23:22:42 -0800

    Broken Checkpoints and Scuffed PlayerController

--- Unity/Assets/PlayerMovement.cs
+++ Unity/Assets/PlayerMovement.cs
@@ -4,23 +4,30 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public CharacterController PlayerController;
-    public Vector3 moveDirection = Vector3.zero;
-    public float gravity = 100f;
-
+    private CharacterController controller;
+    
+    public float speed;
+    public float moveSmoothTime = .3f;
+    Vector2 currentDir = Vector2.zero;
+    Vector2 currentDirVelocity = Vector2.zero;
 
     // Start is called before the first frame update
     void Start()
     {
-        PlayerController = GetComponent<CharacterController>();
+        controller = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(PlayerController.isGrounded == true)
-        {
+        Vector2 targetDir = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        //normalize and smooth vector
+        targetDir.Normalize();
 
-        }
+        currentDir = Vector2.SmoothDamp(currentDir, targetDir, ref currentDirVelocity, moveSmoothTime);
+
+        Vector3 velocity = (transform.forward * currentDir.y + transform.right * currentDir.x) * speed;
+        controller.Move(velocity * Time.deltaTime);
     }
+
 }
--- Unity/Assets/TrackCheckpoints.cs
+++ Unity/Assets/TrackCheckpoints.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class TrackCheckpoints : MonoBehaviour
 {
     private List<CheckpointSingle> checkpointList;
+    private int nextCheckpointIndex = 0;
+    public int laps = 0;
 
     private void Awake()
     {
@@ -21,6 +23,24 @@ public class TrackCheckpoints : MonoBehaviour
 
     public void CheckpointReached(CheckpointSingle checkpoint)
     {
+        //right checkpoint
+        if(checkpointList.IndexOf(checkpoint) == nextCheckpointIndex)
+        {
+            Debug.Log("Right");
+            nextCheckpointIndex = (nextCheckpointIndex + 1)%checkpointList.Count;
+            //make checkpoint sound
 
+            if (nextCheckpointIndex == 1)
+            {
+                laps++;
+            }
+        }
+        //wrong checkpoint
+        else
+        {
+            Debug.Log("Wrong");
+            //wrong sound maybe?
+
+        }
     }
 }
